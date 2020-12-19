6f1f90dac1e7f2bb084949d26c28e25acd82f0d3
ponchione <mitchell.ponchione@protonmail.com>
2020-12-19 10:52:15 -0500

    Added change parsing and int conversion

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-use std::env;
-use std::fs;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 #[macro_use]
 extern crate clap;
 use clap::App;
+use rusty_money::{money, Money};
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     let input = matches.value_of("INPUT").unwrap();
 
-    println!("Using input file: {}", input);
+    println!("INFO! Using input file: {}", input);
     match matches.value_of("divisor") {
         Some(divisor) => {
-            println!("Using divisor: {}", divisor)
+            println!("INFO! Using divisor: {}", divisor)
         },
         _ => {
-            println!("No divisor specified, defaulting to 3.");
+            println!("INFO! No divisor specified, defaulting to 3.");
         }
     }
 
@@ -26,15 +25,28 @@ fn main() {
 
     for line in reader.lines() {
 
-        let _line = line.unwrap();
+        let _line = line.unwrap(); //to keep obj alive
         let data: Vec<&str> = _line.split(",").collect();
-        let owed = data[0];
-        let paid = data[1];
+        // let owed = data[0];
+        // let paid = data[1];
 
-        println!("Owed: {}, Paid: {}", owed, paid);
+        let owed = money!(data[0], "USD");
+        let paid = money!(data[1], "USD");
+        // println!("Owed: {}, Paid: {}", owed, paid);
+        let change = paid - owed;
+        let cents = change.to_string()
+            .replace("$", "")
+            .replace(".", "")
+            .parse::<i32>()
+            .unwrap();
+
+        println!("{}", cents);
+
     }
-
-
 
 }
 
+// fn divisible_3(change: String) -> bool {
+//     let whole_change =
+//     let digit_vec: Vec<>
+// }
